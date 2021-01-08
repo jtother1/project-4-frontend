fe760dbb92b7629e81e7d27a65aa831c446482d5
jtother <juanramonquiroz15@gmail.com>
2021-01-07 22:16:22 -0600

    adjusted css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import {Card, Button} from "react-bootstrap";
 
 class About extends Component {
   render() {
     return (
-      <p>
-        Ringo and Friends was made in order to give all dog owners a forum to
-        post any advice, recommendations, and just plain fun! Feel free to share
-        your stories of your best friend or friends.
-      </p>
+      <Card className="text-center">
+  <Card.Header>About</Card.Header>
+  <Card.Body>
+    <Card.Title>Ringo and Friends</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+  </Card.Body>
+</Card>
     );
   }
 }
